a38b51082222afe7028e503c03cf8eec5dbafff2
xXAlphaManXx <xalphamanx@tuta.io>
2020-07-14 14:50:33 +0530

    Seperate the create-channel into a function

--- commands/createchannel.js
+++ commands/createchannel.js
@@ -1,60 +1,17 @@
 require('dotenv').config();
 const { Message } = require('discord.js');
-const Discord = require('discord.js');
+const createChannel = require('../helpers/create-channel');
 
 /**
  * 
  * @param {Message} message 
  * @param {*} args
  */
-  
 const run = (message, args) => {
-    let categoryName = 'Coffee Buddy';
-    let guild = message.client.guilds.cache.get(process.env.GUILD_ID);
-    let category = guild.channels.cache.find(c => c.name == categoryName && c.type == 'category');
-    let userID1 = '638473299072122900'
-    let userID2 = '732503637296939058'
-    let permissionOverwrites = [
-        {
-            id: userID1,
-            allow: ['VIEW_CHANNEL'],        
-        },
-        {
-            id: userID2,
-            allow: ['VIEW_CHANNEL'],        
-        }
-    ];
-    
-    if (category) {
-        guild.channels.create('coffee-buddy-meet', 
-        { 
-            type: 'text',
-            parent: category,
-            permissionOverwrites: permissionOverwrites,
-            reason: 'Coffee buddies!' 
-        }).then(channel => {
-            // Disallow Everyone to see, join, invite, or speak
-            channel.updateOverwrite(channel.guild.roles.everyone, {
-                'VIEW_CHANNEL': false
-            });
-        })
+    const userID1 = '638473299072122900';
+    const userID2 = '732503637296939058';
 
-        guild.channels.create('coffee-buddy-meet', 
-        { 
-            type: 'voice',
-            parent: category,
-            permissionOverwrites: permissionOverwrites,
-            reason: 'Coffee buddies!' 
-        }).then(channel => {
-            // Disallow Everyone to see, join, invite, or speak
-            channel.updateOverwrite(channel.guild.roles.everyone, {
-                'VIEW_CHANNEL': false
-            });
-        })
-    } else {
-        console.log('No category found to add channel to!')
-        // guild.channels.create(categoryName, { type: 'category', reason: 'New channel added for fun!' });
-    }    
+    return createChannel(message.client, userID1, userID2);
 }
 
 module.exports = {
---
+++ helpers/create-channel.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const { Client } = require('discord.js');
+
+/**
+ * 
+ * @param {Client} client 
+ * @param {string} userOne
+ * @param {string} userTwo
+ */
+const createChannel = async (client, userOne, userTwo) => {
+    const parentName = 'Coffee Buddy';
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+    const parent = guild.channels.cache.find(c => c.name == parentName && c.type == 'category');
+    const permissionOverwrites = [
+        {
+            id: userOne,
+            allow: ['VIEW_CHANNEL'],        
+        },
+        {
+            id: userTwo,
+            allow: ['VIEW_CHANNEL'],        
+        }
+    ];
+
+    if (!parent) {
+        return console.log('No category found to create channels on');
+    }
+
+    const textChannel = await guild.channels.create('coffee-buddy-meet', { 
+        parent,
+        type: 'text',
+        permissionOverwrites,
+        reason: 'Coffee buddies!'
+    });
+    textChannel.updateOverwrite(textChannel.guild.roles.everyone, {
+        'VIEW_CHANNEL': false
+    });
+
+    const voiceChannel = await guild.channels.create('coffee-buddy-meet', { 
+        parent,
+        type: 'voice',
+        permissionOverwrites,
+        reason: 'Coffee buddies!'
+    });
+    voiceChannel.updateOverwrite(textChannel.guild.roles.everyone, {
+        'VIEW_CHANNEL': false
+    });
+}
+
+module.exports = createChannel;
