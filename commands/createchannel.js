b376a79418989b6f4618c3334ad3ee9575d20ab4
Cesare De Cal <hi@cesare.io>
2020-07-14 10:53:43 +0200

    Creating voice channel, setting permissions

--- commands/createchannel.js
+++ commands/createchannel.js
@@ -12,8 +12,45 @@ const run = (message, args) => {
     let categoryName = 'Coffee Buddy';
     let guild = message.client.guilds.cache.get(process.env.GUILD_ID);
     let category = guild.channels.cache.find(c => c.name == categoryName && c.type == 'category');
+    let userID1 = '638473299072122900'
+    let userID2 = '732503637296939058'
+    let permissionOverwrites = [
+        {
+            id: userID1,
+            allow: ['VIEW_CHANNEL'],        
+        },
+        {
+            id: userID2,
+            allow: ['VIEW_CHANNEL'],        
+        }
+    ];
+    
     if (category) {
-        guild.channels.create('coffee-buddy-meet', { type: 'text', parent: category, reason: 'New channel added for fun!' });
+        guild.channels.create('coffee-buddy-meet', 
+        { 
+            type: 'text',
+            parent: category,
+            permissionOverwrites: permissionOverwrites,
+            reason: 'Coffee buddies!' 
+        }).then(channel => {
+            // Disallow Everyone to see, join, invite, or speak
+            channel.updateOverwrite(channel.guild.roles.everyone, {
+                'VIEW_CHANNEL': false
+            });
+        })
+
+        guild.channels.create('coffee-buddy-meet', 
+        { 
+            type: 'voice',
+            parent: category,
+            permissionOverwrites: permissionOverwrites,
+            reason: 'Coffee buddies!' 
+        }).then(channel => {
+            // Disallow Everyone to see, join, invite, or speak
+            channel.updateOverwrite(channel.guild.roles.everyone, {
+                'VIEW_CHANNEL': false
+            });
+        })
     } else {
         console.log('No category found to add channel to!')
         // guild.channels.create(categoryName, { type: 'category', reason: 'New channel added for fun!' });
