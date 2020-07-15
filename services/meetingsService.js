fe056321f096fa150b3d0b8e02c0fd4fe3944a59
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-07-15 18:21:44 +0100

    implemented method to get all meetings for a user

--- services/meetingsService.js
+++ services/meetingsService.js
@@ -4,6 +4,22 @@ const Sequelize = require('sequelize');
 const { Meetings } = require('../models');
 
 class MeetingService extends BaseService {
+    createMeeting = async (discordId1, discordId2) => {
+        console.log(discordId1 + ' ' + discordId2);
+        const meeting = await this.create({ discordId1, discordId2 });
+        return meeting;
+    };
+
+    getUsersMeetings = async (id) => {
+        try {
+            const meetings1 = await this.findAll({ discordId1: id });
+            const meetings2 = await this.findAll({ discordId2: id });
+            return meetings1.concat(meetings2);
+        } catch (error) {
+            return false;
+        }
+    };
+
     //function that checks if user1 has a meeting set-up
     assertUserIsScheduled = async (discordId) => {
         //might need something to check if user is scheduled?
@@ -11,29 +27,51 @@ class MeetingService extends BaseService {
 
     //function that checks if user1 has met with user2 before
     uniqueMeeting = async (discordId) => {
-        try {     //if not, they are a match
-            const user
-
-        } catch (error) {           //if so find another user2
-            return;
-        }
+        // try {     //if not, they are a match
+        //     const user
+        // } catch (error) {           //if so find another user2
+        //     return;
+        // }
     };
 
     //function the prints all meetings for 1 user
-    printSchedule = async (discordId) => {
-        include:{
-            model: model.Meetings 
-            model: model.User
-        }
-        console.log( "result=====>",result);
-        result.forEach((user, index, arr) => {
-            console.log('User: ' + user.username + '\n' + user.linkedin + '\n' + user.github + '\n' + user.funfact );
-            user.project.forEach((projects, index, arr) => {
-                console.log('ID: "' + project.title + '": ' + project.description + '\n');
-            });
+    getUserMeetings = async (id) => {
+        const allowedFields = ['id', 'discordId1', 'discordId2'];
+        try {
+            const meetings1 = await this.findAll(
+                {
+                    discordId1: id,
+                },
+                allowedFields
+            );
+            const meetings2 = await this.findAll(
+                {
+                    discordId2: id,
+                },
+                allowedFields
+            );
+            return meetings1.concat(meetings2);
+        } catch (error) {
+            return false;
         }
     };
 
     //function that updates the meeting table in the DB
 }
-module.exports = { MeetingService};
+module.exports = { MeetingService };
+
+//sample
+// svc = new MeetingService(Meetings);
+// const xyz = async (id) => {
+//     try {
+//         const meetings = await svc.getUserMeetings('40');
+//         console.log(meetings);
+//         // for (let i = 1; i < 50; i++) {
+//         //     await svc.createMeeting(i.toString(), (i + 1).toString());
+//         // }
+//     } catch (e) {
+//         throw e;
+//     }
+// };
+
+// xyz('test');
