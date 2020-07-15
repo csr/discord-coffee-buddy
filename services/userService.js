45bed11e76a01547dc51afb821597e29a8957b51
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-07-15 08:53:26 +0100

    fixed bug involving asserting a user is registered

--- services/userService.js
+++ services/userService.js
@@ -2,18 +2,26 @@
 // const { User } = sequelize.models;
 const { User } = require('../models');
 const { BaseService } = require('./baseService');
+const Sequelize = require('sequelize');
 class UserService extends BaseService {
     assertUserIsRegistered = async (discordId) => {
-        try {
-            await this.findOne({ discordId });
-        } catch (e) {
+        const user = await this.findOne({ discordId });
+        if (!user && !user.enrolled) {
             throw new Error(
-                "It seems you're not registered. Type `!start` to register right away!"
+                "It seems you're not enrolled. Type `!start` to enroll right away!"
             );
         }
     };
+    prettifyError = (error) => {
+        if (error instanceof Sequelize.UniqueConstraintError) {
+            return new Error(
+                `You've already created an account! Try the other commands!`
+            );
+        }
+        return error;
+    };
     updateByDiscordId = async (discordId, updateBody) => {
-        await this.assertUserIsRegistered();
+        await this.assertUserIsRegistered(discordId);
         const updatedBody = await this.update(updateBody, { discordId });
         return updatedBody;
     };
