0feb9c8a1427a032a41e21ef78f7038bd28c40c2
Kristi <kltakach@gmail.com>
2020-07-14 20:16:33 -0400

    able to see this update in github URL in DB

--- commands/github.js
+++ commands/github.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
 const { Message } = require('discord.js');
+const { UserService } = require('../services/userService.js');
+const { User } = require('../models');
+
+const svc = new UserService(User);
 
 /**
  * 
  * @param {Message} message 
  * @param {*} args
  */
-const run = (message, args) => {
-    message.author.send('Yay! We have set your github profile to ' + args);
+const run = async (message, args) => {
+    try {
+        await svc.updateByDiscordId(message.author.id, {
+            github: args,
+        });
+        message.author.send('Yay! We have set your github profile to ' + args);
+    } catch (error) {
+        message.author.send(error.message);
+    }
 }
 
 module.exports = {
