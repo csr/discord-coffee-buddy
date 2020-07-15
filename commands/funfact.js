40bbe0800853c95f496bf5e620c8827e30829668
Kristi <kltakach@gmail.com>
2020-07-14 20:24:12 -0400

    got funfact to save something in DB

--- commands/funfact.js
+++ commands/funfact.js
@@ -1,14 +1,25 @@
 require('dotenv').config();
 const { Message } = require('discord.js');
+const { UserService } = require('../services/userService.js');
+const { User } = require('../models');
+
+const svc = new UserService(User);
 
 /**
  * 
  * @param {Message} message 
  * @param {*} args
  */
-const run = (message, args) => {
-    message.author.send('Yay! We have set your funfact as ' + args);
-}
+const run = async (message, args) => {
+    try {
+        await svc.updateByDiscordId(message.author.id, {
+            funfact: args,
+        });
+        message.author.send('Yay! We have set your funfact as ' + args);
+    } catch (error) {
+        message.author.send(error.message);
+    }
+};
 
 module.exports = {
     run,
