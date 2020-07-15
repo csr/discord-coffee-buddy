bed10a24cd15ca77a3a751a673a3cb8f8449cccf
Kristi <kltakach@gmail.com>
2020-07-14 20:20:02 -0400

    able to see this save LI URL in DB

--- commands/linkedin.js
+++ commands/linkedin.js
@@ -1,14 +1,25 @@
 require('dotenv').config();
 const { Message } = require('discord.js');
+const { UserService } = require('../services/userService.js');
+const { User } = require('../models');
+
+const svc = new UserService(User);
 
 /**
  * 
  * @param {Message} message 
  * @param {*} args
  */
-const run = (message, args) => {
-    message.author.send('Yay! We have set your linkedin profile to ' + args);
-}
+const run = async (message, args) => {
+    try {
+        await svc.updateByDiscordId(message.author.id, {
+            linkedin: args,
+        });
+        message.author.send('Yay! We have set your linkedin profile to ' + args);
+    } catch (error) {
+        message.author.send(error.message);
+    }
+};
 
 module.exports = {
     run,
