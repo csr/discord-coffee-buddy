8ac5561a9bc4c3aac7be8c8e2e467f68a64e80e7
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-07-15 18:28:29 +0100

    cleaned up meetingService

--- services/meetingsService.js
+++ services/meetingsService.js
@@ -9,32 +9,6 @@ class MeetingService extends BaseService {
         const meeting = await this.create({ discordId1, discordId2 });
         return meeting;
     };
-
-    getUsersMeetings = async (id) => {
-        try {
-            const meetings1 = await this.findAll({ discordId1: id });
-            const meetings2 = await this.findAll({ discordId2: id });
-            return meetings1.concat(meetings2);
-        } catch (error) {
-            return false;
-        }
-    };
-
-    //function that checks if user1 has a meeting set-up
-    assertUserIsScheduled = async (discordId) => {
-        //might need something to check if user is scheduled?
-    };
-
-    //function that checks if user1 has met with user2 before
-    uniqueMeeting = async (discordId) => {
-        // try {     //if not, they are a match
-        //     const user
-        // } catch (error) {           //if so find another user2
-        //     return;
-        // }
-    };
-
-    //function the prints all meetings for 1 user
     getUserMeetings = async (id) => {
         const allowedFields = ['id', 'discordId1', 'discordId2'];
         try {
@@ -55,8 +29,6 @@ class MeetingService extends BaseService {
             return false;
         }
     };
-
-    //function that updates the meeting table in the DB
 }
 module.exports = { MeetingService };
 
