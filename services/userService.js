1d784b74946da2a688daca1ad35d9404585e2d2f
Cesare De Cal <hi@cesare.io>
2020-07-16 16:30:57 +0200

    Getting prefix from environment variable

--- services/userService.js
+++ services/userService.js
@@ -3,12 +3,14 @@
 const { User } = require('../models');
 const { BaseService } = require('./baseService');
 const Sequelize = require('sequelize');
+
+const prefix = process.env.PREFIX || '!';
 class UserService extends BaseService {
     assertUserIsRegistered = async (discordId) => {
         const user = await this.userExists(discordId);
         if (!user || !user.enrolled) {
             throw new Error(
-                "It seems you're not enrolled. Type `!start` to enroll right away!"
+                `It seems you're not enrolled. Type \`${prefix + 'start'}\` to enroll right away!`
             );
         }
     };
