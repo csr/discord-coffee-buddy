f99f4c9bf9adcd2005c421dba24b7e8103efec46
xXAlphaManXx <xalphamanx@tuta.io>
2020-07-16 18:02:20 +0530

    Nearly completed the scheduler.
    Known bug fixing and changes pending

--- helpers/scheduler/index.js
+++ helpers/scheduler/index.js
@@ -1,16 +1,52 @@
 require('dotenv').config();
-const { Client, Guild } = require('discord.js');
+const { Client } = require('discord.js');
+const { User } = require('../../models');
 const createChannel = require('./../create-channel');
+const { UserService } = require('./../../services/userService');
 
-const getRandomUsers = () => {
-    // Call the db, get list of users, create random pairs
-    // And return it as an object / array
+const svc = new UserService(User);
 
-    // Something like this;
-    return [
-        ['638473299072122900', '229858084493721600'],
-        ['484101310870847518', '484739100604235786']
-    ];
+/**
+ * @see https://stackoverflow.com/a/44996257
+ * @param {string[]} array
+ */
+const pairArray = (array) => {
+    return array.reduce(function(result, value, index, array) {
+        if (index % 2 === 0)
+            result.push(array.slice(index, index + 2));
+        return result;
+    }, []);
+}
+
+/**
+ * @see https://stackoverflow.com/a/12646864
+ * @param {string[]} array 
+ */
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
+
+const getRandomUsers = async () => {
+    let userIDSet = [];
+    const users = await svc.findAll();
+
+    if (!Object.keys(users).length) {
+        console.log('DAMN! There are no users enrolled yet. What a bummer!');
+    }
+
+    for (const user of users) {
+        userIDSet.push(user.discordId);
+    }
+
+    const shuffledUserIDs = shuffleArray(userIDSet);
+    const pairedUserIDs = pairArray(shuffledUserIDs);
+
+    return pairedUserIDs;
 }
 
 /**
@@ -20,10 +56,14 @@ const getRandomUsers = () => {
 const runScheduler = async (client) => {
     console.log('Running the scheduler. Time to match people up. Yuhu!');
 
-    const guild = client.guilds.cache.get(process.env.GUILD_ID);
-    const users = getRandomUsers();
+    const users = await getRandomUsers();
 
+    // FIXME: There is a serious bug invovled which needs to be fixed ASAP
+    // If the array that is being returned is not a pair, then this function would fail
+    // We need to handle this edge case and report it gracefully
     for (const pair of users) {
+        // TODO: Save into meetings category
+        // For now just creates the channels required.
         createChannel(client, pair[0], pair[1])
     }
 }
