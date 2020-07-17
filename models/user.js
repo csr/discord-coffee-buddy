7a617f2136a97140a93f1d7314b484812028f7c0
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-07-17 10:12:48 +0100

    added new migration to rename the columns

--- models/user.js
+++ models/user.js
@@ -1,31 +1,29 @@
-const { Model } = require('sequelize');
-
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class User extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-        }
-    };
-
-    User.init({
-        username: DataTypes.STRING,
-        discordId: DataTypes.STRING,
-        linkedin: DataTypes.STRING,
-        github: DataTypes.STRING,
-        funfact: DataTypes.STRING,
-        pronouns: DataTypes.STRING,
-        enrolled: DataTypes.BOOLEAN
-    }, {
-        sequelize,
-        modelName: 'User',
-    });
-
-    return User;
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    username: DataTypes.STRING,
+    discordId: DataTypes.STRING,
+    linkedin: DataTypes.STRING,
+    github: DataTypes.STRING,
+    bio: DataTypes.STRING,
+    pronouns: DataTypes.STRING,
+    enrolled: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
 };
-
-
