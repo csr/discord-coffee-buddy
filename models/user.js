c002207c7d5457eb12db4fd1dfdffd18ce76c1eb
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-07-17 09:23:00 +0100

    rolled back migration file and user model to initial state

--- models/user.js
+++ models/user.js
@@ -1,28 +1,31 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
 
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    username: DataTypes.STRING,
-    discordId: DataTypes.STRING,
-    linkedin: DataTypes.STRING,
-    github: DataTypes.STRING,
-    bio: DataTypes.STRING,
-    pronouns: DataTypes.STRING,
-    enrolled: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+    User.init({
+        username: DataTypes.STRING,
+        discordId: DataTypes.STRING,
+        linkedin: DataTypes.STRING,
+        github: DataTypes.STRING,
+        funfact: DataTypes.STRING,
+        pronouns: DataTypes.STRING,
+        enrolled: DataTypes.BOOLEAN
+    }, {
+        sequelize,
+        modelName: 'User',
+    });
+
+    return User;
 };
+
+
